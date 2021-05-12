f73a3773ebdaad949bbe589f80ba2675a7dfaba3
4gboframram <79847791+4gboframram@users.noreply.github.com>
2021-05-11 22:48:12 -0400

    Delete Pyubiomes/searches directory

--- Pyubiomes/searches/setup.py
+++
@@ -1,3 +0,0 @@
-from distutils.core import setup, Extension
-setup(name = 'searches', version = '0.1',author="", url="", description="search functions for Pyubiomes",
-   ext_modules = [Extension('searches', ['wrap.c'])])
